3351c1004bea90e0c175093fc77edcd205aebc58
Piseth Ing <piseth.ing@allianz.com>
2023-02-01 18:01:13 +0700

    add excercise 2

---
+++ excercise-2/src/test/java/com/piseth/java/school/excercise2/Excercise2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.piseth.java.school.excercise2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Excercise2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
